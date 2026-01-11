66979c2ed5a4b3d03e0266ee58e517f42c0e58b4
johngrindall <john@2simple.com>
2026-01-11 17:28:25 +0000

    finish exercise 3

--- src/TrafficObject.cpp
+++ src/TrafficObject.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <chrono>
 #include <algorithm>
+#include <mutex>
 #include "TrafficObject.h"
 
 // init static variable
 int TrafficObject::_idCnt = 0;
+std::mutex TrafficObject::_mtxCout;
 
-// L3.2 : Add a static mutex to the base class TrafficObject (called _mtxCout) and properly instantiate it in the source file. This mutex will be used in the next task to protect standard-out. 
+
+
+// L3.2 : Add a static mutex to the base class TrafficObject (called _mtxCout)
+//and properly instantiate it in the source file. This mutex will be used in the next task to protect standard-out. 
 
 void TrafficObject::setPosition(double x, double y)
 {
